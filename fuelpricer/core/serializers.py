4433c82e5e80b251fe84b8785cbf78722e2e05b4
Andy Nguyencuu <andy.009876@gmail.com>
2021-03-18 18:11:11 -0500

    fixed serializer to hash password on creation

--- fuelpricer/core/serializers.py
+++ fuelpricer/core/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from .models import CustomUser
 
@@ -13,13 +14,13 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields = ('username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            password = validated_data.pop('password', None)
-            instance = self.model(**validated_data)  # used to be self.Meta.model changed to self.model
-            if password is not None:
-                instance.set_password(password)
-            instance.save()
-            return instance
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)  # used to be self.Meta.model changed to self.model
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
@@ -30,3 +31,9 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         # Add custom claims
         token['fullname'] = user.fullname
         return token
+
+def validate_password(self, value: str) -> str:
+    """
+    Hashes passwords
+    """
+    return make_password(value)
