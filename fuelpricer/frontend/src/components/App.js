2e3cb9202a35e101560570f7fa46d8b8d05f8241
tmyou <youtim000@gmail.com>
2021-03-19 01:10:15 -0500

    attempted bug fix

--- fuelpricer/frontend/src/components/App.js
+++ fuelpricer/frontend/src/components/App.js
@@ -1,39 +1,23 @@
-import React, {Component} from "react";
+import React, { Component} from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import "./icons.js";
-import Dashboard from "./Dashboard";
-import FuelQuoteForm from "./FuelQuoteForm";
-import FuelQuoteHistory from "./FuelQuoteHistory";
-import ProfileManagement from "./ProfileManagement";
-import Registration from "./Registration";
-import Splash from "./Splash";
+import Login from "./login";
+import Signup from "./signup";
 
 class App extends Component {
-  render() {
-    return (
-  
-      <div className="site">  
-        <nav>
-          <Link className={"nav-link"} to={"/"}>Home</Link>
-          <Link className={"nav-link"} to={"/Splash/"}>Login</Link>
-          <Link className={"nav-link"} to={"/Registration/"}>Signup</Link>
-        </nav>
-        <main>
-          <Switch>
-            {/* <Route path={"/"} render={() => <div>Home again</div>} /> */}
-            <Route exact path={"/Dashboard/"} component={Dashboard} />
-            <Route exact path={"/FuelQuoteForm/"} component={FuelQuoteForm} />
-            <Route exact path={"/FuelQuoteHistory/"} component={FuelQuoteHistory} />
-            <Route exact path={"/ProfileManagement/"} component={ProfileManagement} />
-            <Route exact path={"/Registration/"} component={Registration} />
-            <Route exact path={"/Splash/"} component={Splash} />
-            <Route path={"/"} component={Splash} />
-          </Switch>
-        </main>
-      </div>
-
-    );
-  }
+    render() {
+        return (
+            <div className="site">
+                <main>
+                    <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
+                    <Switch>
+                        <Route exact path={"/login/"} component={Login}/>
+                        <Route exact path={"/signup/"} component={Signup}/>
+                        <Route path={"/"} render={() => <div>Home again</div>}/>
+                   </Switch>
+               </main>
+            </div>
+        );
+    }
 }
 
 export default App;
