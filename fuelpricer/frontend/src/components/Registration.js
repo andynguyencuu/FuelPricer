26c7008ca27649bc96ea614ecd33528642c11c80
Andy Nguyencuu <andy.009876@gmail.com>
2021-04-07 02:48:42 -0500

    registration → asynchronous + inline error handline

--- fuelpricer/frontend/src/components/Registration.js
+++ fuelpricer/frontend/src/components/Registration.js
@@ -5,203 +5,204 @@ import TextPassword from "./TextPassword";
 import TextPasswordConf from "./TextPasswordConf";
 import { Link } from "react-router-dom";
 import ButtonSmallBlue from "./ButtonSmallBlue";
+import InlineError from "./InlineError"
 import axios from "axios";
+import axiosInstance from "../axiosApi";
 
 class Registration extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      passwordconf: "",
-  };
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: "",
+			password: "",
+			passwordconf: "",
+			error: false,
+			error_msg: ""
+		};
 
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-  }
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-  handleChange(event) {
-    this.setState({[event.target.name]: event.target.value});
-  }
+	handleChange(event) {
+		this.setState({ [event.target.name]: event.target.value });
+	}
 
-  handleSubmit(event) {
-    event.preventDefault();
-    axios({           // CREATE USER
-      method: "post",
-      url: "http://localhost:8000/api/user/create/",
-      data: {username: this.state.username, password: this.state.password},
-      headers: {        
-        'Authorization': "JWT " + localStorage.getItem('access_token'),
-        'Content-Type': 'application/json',
-        'accept': 'application/json' },
-      })
-      .then(function (response) {
-        //handle success
-        console.log(response);
-      })
-      .catch(function (response) {
-        //handle error
-        alert(response);
-      });
-    setTimeout(()=> axios({           // USE TO SIGN IN
-      method: "post",
-      url: "http://localhost:8000/api/token/obtain/",
-      data: { username: this.state.username, password: this.state.password },
-      headers: {
-        'Authorization': "JWT " + localStorage.getItem('access_token'),
-        'Content-Type': 'application/json',
-        'accept': 'application/json'
-      },
-    })
-      .then(function (response) {
-        localStorage.setItem('access_token', response.data.access);
-        localStorage.setItem('refresh_token', response.data.refresh);
-        //handle success
-        //Change later
-        window.location.replace('http://localhost:8000/Profile/')
-      })
-      .catch(function (response) {
-        //handle error
-        console.log(response);
-      }), 1000)
+	handleError(err) {
+		this.setState({ ['error']: true, ['error_msg']: err });
+		return setTimeout(function () { this.setState({ ['error']: false }) }.bind(this), 4000);
+	}
 
-  }
+	async handleSubmit(event) {
+		event.preventDefault();
+		if (this.state.username == "") return this.handleError("Please provide a user ID!");
+		if (this.state.password == "") return this.handleError("Please provide a password!");
+		if (this.state.password != this.state.passwordconf) return this.handleError("Passwords must match!");
+		try {		// CREATE USER
+			await axiosInstance.post('/user/create/', {
+			username: this.state.username, 
+			password: this.state.password
+			}, {method: 'post'});
+		}
+		catch {
+			return this.handleError("Server error creating user.");
+		} // may still need 1000 timeout?
+		try {		// USE TO SIGN IN
+			const response = await axiosInstance.post('/token/obtain/', {
+				username: this.state.username,
+				password: this.state.password
+			}, { method: 'post' });
+			localStorage.setItem('access_token', response.data.access);
+			localStorage.setItem('refresh_token', response.data.refresh);
+			axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
 
-  render() {
-    return (
-      <Container
-        style={{
-          backgroundImage: `linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(242,213,153,1) 89%)`
-        }}
-      >
-        <Logo src={"https://i.ibb.co/bFRMRGm/fuel23.png"}></Logo>
-        <RegisterDialog>
-          <BoxHeader>
-            <RegisterAccount>REGISTER ACCOUNT</RegisterAccount>
-          </BoxHeader>
-          <form onSubmit={this.handleSubmit}>
-          <TextUser
-            inputStyle="Label"
-            style={{
-              width: 300,
-              height: 35,
-              marginRight: 20,
-              marginLeft: 20,
-              marginBottom: 10
-            }}
-            inputStyle="User ID"
-            name="username" type="text" value={this.state.username} onChange={this.handleChange}
-          ></TextUser>
-            <TextPassword secureTextEntry={true}
-            style={{
-              width: 300,
-              height: 35,
-              marginRight: 20,
-              marginLeft: 20,
-              marginBottom: 10
-            }}
-            name="password" type="password" value={this.state.password} onChange={this.handleChange}
-          ></TextPassword>
-          <TextPasswordConf
-            inputStyle="Password"
-            style={{
-              width: 300,
-              height: 35,
-              marginRight: 20,
-              marginLeft: 20,
-              marginBottom: 20
-            }}
-            name="passwordconf" type="password" value={this.state.passwordconf} onChange={this.handleChange}
-            inputStyle="Confirm Password"
-          ></TextPasswordConf>
-            <Button type="submit" value="Register">
-              <ButtonOverlay>
-                <ButtonSmallBlue
-                  style={{
-                    width: 100,
-                    height: 44,
-                    marginBottom: 100,
-                    marginLeft: 95,
-                    backgroundColor: "rgba(219,87,147,1)"
-                  }}
-                  button="Register"
-                ></ButtonSmallBlue>
-              </ButtonOverlay>
-            </Button>
-            </form>
-        </RegisterDialog>
-        <br></br>
-        <Container></Container>
-      </Container>
-    );
-  }
+			window.location.replace('http://localhost:8000/Profile/');
+		} catch (err) {
+			if (err.response) {
+				if (err.response.status >= 500) return this.handleError("Server error.");
+			}
+			else return this.handleError("Unexpected server error.")
+		}
+	}
+
+	render() {
+		return (
+			<Container
+				style={{
+					backgroundImage: `linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(242,213,153,1) 89%)`
+				}}
+			>
+				<Logo src={"https://i.ibb.co/bFRMRGm/fuel23.png"}></Logo>
+				<RegisterDialog>
+					<BoxHeader>
+						<RegisterAccount>REGISTER ACCOUNT</RegisterAccount>
+					</BoxHeader>
+					<form onSubmit={this.handleSubmit}>
+						<TextUser
+							inputStyle="Label"
+							style={{
+								width: 300,
+								height: 35,
+								marginRight: 20,
+								marginLeft: 20,
+								marginBottom: 10
+							}}
+							inputStyle="User ID"
+							name="username" type="text" value={this.state.username} onChange={this.handleChange}
+						></TextUser>
+						<TextPassword secureTextEntry={true}
+							style={{
+								width: 300,
+								height: 35,
+								marginRight: 20,
+								marginLeft: 20,
+								marginBottom: 10
+							}}
+							name="password" type="password" value={this.state.password} onChange={this.handleChange}
+						></TextPassword>
+						<TextPasswordConf
+							inputStyle="Password"
+							style={{
+								width: 300,
+								height: 35,
+								marginRight: 20,
+								marginLeft: 20,
+								marginBottom: 20
+							}}
+							name="passwordconf" type="password" value={this.state.passwordconf} onChange={this.handleChange}
+							inputStyle="Confirm Password"
+						></TextPasswordConf>
+						<InlineError error={this.state.error}>
+							{this.state.error_msg}
+						</InlineError>
+						<Button type="submit" value="Register">
+							<ButtonOverlay>
+								<ButtonSmallBlue
+									style={{
+										width: 100,
+										height: 44,
+										marginBottom: 100,
+										marginLeft: 95,
+										backgroundColor: "rgba(219,87,147,1)"
+									}}
+									button="Register"
+								></ButtonSmallBlue>
+							</ButtonOverlay>
+						</Button>
+					</form>
+				</RegisterDialog>
+				<br></br>
+				<Container></Container>
+			</Container>
+		);
+	}
 }
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-  margin: -8px -8px -8px -8px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+	width: 100vw;
+	margin: -8px -8px -8px -8px;
 `;
 
 const ButtonOverlay = styled.button`
-  display: block;
-  background: none;
-  height: 100%;
-  width: 100%;
-  border: none;
+	display: block;
+	background: none;
+	height: 100%;
+	width: 100%;
+	border: none;
 `;
 const Logo = styled.img`
-  width: 174px;
-  height: 100%;
-  margin-top: 50px;
-  object-fit: contain;
+	width: 174px;
+	height: 100%;
+	margin-top: 50px;
+	object-fit: contain;
 `;
 
 const RegisterDialog = styled.div`
-  flex-direction: column;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 30px;
-  align-items: center;
-  width: 340px;
-  margin-top: 60px;
-  margin-bottom: 80px;
-  display: flex;
-  box-shadow: 3px 3px 10px 0.5px rgba(0, 0, 0, 1);
+	flex-direction: column;
+	background-color: rgba(255, 255, 255, 1);
+	border-radius: 30px;
+	align-items: center;
+	width: 340px;
+	margin-top: 60px;
+	margin-bottom: 80px;
+	display: flex;
+	box-shadow: 3px 3px 10px 0.5px rgba(0, 0, 0, 1);
 `;
 
 const BoxHeader = styled.div`
-  height: 60px;
-  background-color: rgba(219, 87, 147, 1);
-  border-top-left-radius: 30px;
-  border-top-right-radius: 30px;
-  width: 340px;
-  margin: 0px;
-  margin-bottom: 20px;
-  flex-direction: column;
-  display: flex;
-  justify-content: center;
+	height: 60px;
+	background-color: rgba(219, 87, 147, 1);
+	border-top-left-radius: 30px;
+	border-top-right-radius: 30px;
+	width: 340px;
+	margin: 0px;
+	margin-bottom: 20px;
+	flex-direction: column;
+	display: flex;
+	justify-content: center;
 `;
 
 const RegisterAccount = styled.span`
-  font-family: Lato;
-  font-style: normal;
-  font-weight: 900;
-  color: rgba(255, 255, 255, 1);
-  font-size: 20px;
-  align-self: center;
+	font-family: Lato;
+	font-style: normal;
+	font-weight: 900;
+	color: rgba(255, 255, 255, 1);
+	font-size: 20px;
+	align-self: center;
 `;
 
 const Button = styled.div`
-  height: 44px;
-  margin-bottom: 15px;
-  align-self: stretch;
-  align-items: center;
-  flex-direction: column;
-  border: none;
+	height: 44px;
+	margin-bottom: 15px;
+	align-self: stretch;
+	align-items: center;
+	flex-direction: column;
+	border: none;
 `;
 
 export default Registration;
--- fuelpricer/frontend/src/components/Splash.js
+++ fuelpricer/frontend/src/components/Splash.js
@@ -9,122 +9,122 @@ import InlineError from "./InlineError"
 import { axiosInstance } from "../axiosApi";
 
 class Splash extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { username: "", password: "", error: false, error_msg: ""};
+	constructor(props) {
+		super(props);
+		this.state = { username: "", password: "", error: false, error_msg: "" };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
-    }
+	handleChange(event) {
+		this.setState({ [event.target.name]: event.target.value });
+	}
 
-    handleError(err) {
-      this.setState({ ['error']: true, ['error_msg']: err});
-      setTimeout(function () { this.setState({ ['error']: false}) }.bind(this), 4000);
-    }
+	handleError(err) {
+		this.setState({ ['error']: true, ['error_msg']: err });
+		return setTimeout(function () { this.setState({ ['error']: false }) }.bind(this), 4000);
+	}
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        try {
-          const data = await axiosInstance.post('/token/obtain/', {
-            username: this.state.username,
-            password: this.state.password
-          }, {method: 'post'});
-          localStorage.setItem('access_token', data.data.access);
-          localStorage.setItem('refresh_token', data.data.refresh);
-          axiosInstance.defaults.headers['Authorization'] = "JWT " + data.data.access;
+	async handleSubmit(event) {
+		event.preventDefault();
+		try {
+			const data = await axiosInstance.post('/token/obtain/', {
+				username: this.state.username,
+				password: this.state.password
+			}, { method: 'post' });
+			localStorage.setItem('access_token', data.data.access);
+			localStorage.setItem('refresh_token', data.data.refresh);
+			axiosInstance.defaults.headers['Authorization'] = "JWT " + data.data.access;
 
-          // TODO 
-          //  CHECK REQ'D FIELDS FOR EMPTY. IF EMPTY REDIR → PROF MANAGEMENT
-          // Object = ????? 
-          // if (Object.values(this.state).slice(0, 2).concat(Object.values(this.state).slice(3,6)).includes(""))
-          // for if a user quits after registering or something
-          window.location.replace('http://localhost:8000/Dashboard/');
-        } catch (err) {
-          if (err.response){
-            if (err.response.status >= 500) this.handleError("Server error.");
-          }
-          else this.handleError("Invalid user ID/password.")
-          return;
-        }
-    }
+			// TODO 
+			//  CHECK REQ'D FIELDS FOR EMPTY. IF EMPTY REDIR → PROF MANAGEMENT
+			// Object = ????? 
+			// if (Object.values(this.state).slice(0, 2).concat(Object.values(this.state).slice(3,6)).includes(""))
+			// for if a user quits after registering or something
+			window.location.replace('http://localhost:8000/Dashboard/');
+		} catch (err) {
+			if (err.response) {
+				if (err.response.status >= 500) this.handleError("Server error.");
+			}
+			else this.handleError("Invalid user ID/password.")
+			return;
+		}
+	}
 
-    render() {
-        return (
-        <Container
-            style={{
-                backgroundImage: `linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(242,213,153,1) 89%)`
-            }}
-        >
-            <Logo src={"https://i.ibb.co/bFRMRGm/fuel23.png"}></Logo>
-            <SignInDialog>
-                <BoxHeader>
-                    <Header1>ACCOUNT SIGN IN!</Header1>
-                </BoxHeader>
-                <form onSubmit={this.handleSubmit}>
-                <TextUser
-                    name="username" type="text" value={this.state.username} onChange={this.handleChange}
-                    style={{
-                        width: 300,
-                        height: 35,
-                        marginRight: 20,
-                        marginLeft: 20,
-                        marginBottom: 10
-                    }}
-                    ></TextUser> 
-                <TextPassword
-                   name="password" type="password" value={this.state.password} onChange={this.handleChange}
-                    style={{
-                        width: 300,
-                        height: 35,
-                        marginRight: 20,
-                        marginLeft: 20,
-                        marginBottom: 15
-                    }}
-                    ></TextPassword>
-                    <InlineError error={this.state.error}>
-                      {this.state.error_msg}
-                      </InlineError>
-                    <SignIn
-                    type="submit" value="Sign In">
-                        <ButtonOverlay>
-                            <ButtonSmallBlue
-                                style={{
-                                    width: 85,
-                                    height: 44,
-                                    margin: 0,
-                                    marginLeft: 106,
-                                    marginBottom: 0
-                                }}
-                                button="Sign In"
-                                ></ButtonSmallBlue>
-                        </ButtonOverlay>
-                    </SignIn>     
-                <Link to="/Registration">
-                    <Register>
-                        <ButtonOverlay>
-                            <ButtonSmallGrey
-                                style={{
-                                    width: 100,
-                                    height: 44,
-                                    margin: 0,
-                                    marginLeft: 100,
-                                }}
-                                caption="Register"
-                                ></ButtonSmallGrey>
-                        </ButtonOverlay>
-                    </Register>
-                </Link>
-                </form>
-            </SignInDialog>
-            <br></br>
-            <Container></Container>
-        </Container>
-    );
-}
+	render() {
+		return (
+			<Container
+				style={{
+					backgroundImage: `linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(242,213,153,1) 89%)`
+				}}
+			>
+				<Logo src={"https://i.ibb.co/bFRMRGm/fuel23.png"}></Logo>
+				<SignInDialog>
+					<BoxHeader>
+						<Header1>ACCOUNT SIGN IN!</Header1>
+					</BoxHeader>
+					<form onSubmit={this.handleSubmit}>
+						<TextUser
+							name="username" type="text" value={this.state.username} onChange={this.handleChange}
+							style={{
+								width: 300,
+								height: 35,
+								marginRight: 20,
+								marginLeft: 20,
+								marginBottom: 10
+							}}
+						></TextUser>
+						<TextPassword
+							name="password" type="password" value={this.state.password} onChange={this.handleChange}
+							style={{
+								width: 300,
+								height: 35,
+								marginRight: 20,
+								marginLeft: 20,
+								marginBottom: 15
+							}}
+						></TextPassword>
+						<InlineError error={this.state.error}>
+							{this.state.error_msg}
+						</InlineError>
+						<SignIn
+							type="submit" value="Sign In">
+							<ButtonOverlay>
+								<ButtonSmallBlue
+									style={{
+										width: 85,
+										height: 44,
+										margin: 0,
+										marginLeft: 106,
+										marginBottom: 0
+									}}
+									button="Sign In"
+								></ButtonSmallBlue>
+							</ButtonOverlay>
+						</SignIn>
+						<Link to="/Registration">
+							<Register>
+								<ButtonOverlay>
+									<ButtonSmallGrey
+										style={{
+											width: 100,
+											height: 44,
+											margin: 0,
+											marginLeft: 100,
+										}}
+										caption="Register"
+									></ButtonSmallGrey>
+								</ButtonOverlay>
+							</Register>
+						</Link>
+					</form>
+				</SignInDialog>
+				<br></br>
+				<Container></Container>
+			</Container>
+		);
+	}
 }
 
 const Container = styled.div`
