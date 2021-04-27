ff43f1e930124b762c4e297d1ed9f71aef0551ed
tmyou <youtim000@gmail.com>
2021-04-26 23:25:25 -0500

    added test case for test_views
    Co-authored-by: AJams23 <AJams23@users.noreply.github.com>

--- fuelpricer/core/tes2/test_views_2.py
+++ fuelpricer/core/tes2/test_views_2.py
@@ -1,9 +1,48 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from core.models import CustomUser, FuelQuote
 import json
 
 class TestViews(TestCase):
-    def hello_world_test_view(self):
+
+    def setUp(self):
         self.client = Client()
-        response = client.get(reverse('hello_world'))
+        self.createUser = reverse('create_user')
+        self.CustomUser = CustomUser.objects.create(
+            fullname='Imatestcase',
+            address='843 odea blvd',
+            address_2='844 odea blvd',
+            city='Houston',
+            state='TX',
+            zipcode='77777'
+        )
+    
+    def test_CustomUsercreate_POST_add_new_user(self):
+        CustomUser.objects.create(
+            username='djangotestcase',
+            password='djangotestcase',
+            fullname='Imatestcase',
+            address='843 odea blvd',
+            address_2='844 odea blvd',
+            city='Houston',
+            state='TX',
+            zipcode='77777'
+        )
+
+        response = self.client.post(self.createUser, {
+            'username':'djangotestcase2',
+            'password':'djangotestcase2',
+            'fullname':'Imatestcase',
+            'address':'843 odea blvd',
+            'address_2':'844 odea blvd',
+            'city':'Houston',
+            'state':'TX',
+            'zipcode':'77777'
+        })
+        #self.assertEquals(response.status_code, 400)
+        self.assertEquals(response.status_code, 201)
+
+
+    def hello_world_test_view(self):
+        response = self.client.get(reverse('hello_world'))
         self.assertEquals(response.status_code, 200)
