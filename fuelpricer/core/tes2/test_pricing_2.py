f7b60327067cce89176c72c9c1b0507c87d40c2f
Alan Frank <alanfrank1998@gmail.com>
2021-04-27 01:48:57 -0500

    pricing unit test and coverage report fix
    Co-authored-by: Timothy You <tmyou@users.noreply.github.com>

--- fuelpricer/core/tes2/test_pricing_2.py
+++ fuelpricer/core/tes2/test_pricing_2.py
@@ -1,6 +1,8 @@
 from django.test import SimpleTestCase
-from .models import FuelQuote, CustomUser
+from core.pricing import Pricer
+price = Pricer(2, 3, "TX", 1)
+price2 = Pricer(2, 3, "MS", 1)
 
-def test_generate():
-    assert 1 == 1
-    print("hello")
+class TestGenerate(SimpleTestCase):
+    def test_generate_instate(self):
+        self.assertEqual(6.48, price.generate())
