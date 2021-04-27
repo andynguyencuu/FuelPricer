ac69c0b35ddeb9c037268fa2159bcfd6702a7ba1
tmyou <youtim000@gmail.com>
2021-04-26 20:05:34 -0500

    Added State Dropdown Menu
    Co-authored-by: Andy Nguyencuu<andynguyencuu@users.noreply.github.com>
    Co-authored-by: AJams23 <AJams23@users.noreply.github.com>

--- fuelpricer/frontend/src/components/TextState.js
+++ fuelpricer/frontend/src/components/TextState.js
@@ -15,7 +15,61 @@ function TextState(props) {
         }}
       />
 
-      <InputStyle value={props.value} name="state" placeholder="State*" maxLength={2}></InputStyle>
+      <InputStyle value={props.value} name="state" placeholder="State"style={{
+          fontSize: 18,
+        }}>
+        <option value="AL" >AL</option>
+        <option value="AK">AK</option>
+        <option value="AZ">AZ</option>
+        <option value="AR">AR</option>
+        <option value="CA">CA</option>
+        <option value="CO">CO</option>
+        <option value="CT">CT</option>
+        <option value="DE">DE</option>
+        <option value="DC">DC</option>
+        <option value="FL">FL</option>
+        <option value="GA">GA</option>
+        <option value="HI">HI</option>
+        <option value="ID">ID</option>
+        <option value="IL">IL</option>
+        <option value="IN">IN</option>
+        <option value="IA">IA</option>
+        <option value="KS">KS</option>
+        <option value="KY">KY</option>
+        <option value="LA">LA</option>
+        <option value="ME">ME</option>
+        <option value="MD">MD</option>
+        <option value="MA">MA</option>
+        <option value="MI">MI</option>
+        <option value="MN">MN</option>
+        <option value="MS">MS</option>
+        <option value="MO">MO</option>
+        <option value="MT">MT</option>
+        <option value="NE">NE</option>
+        <option value="NV">NV</option>
+        <option value="NH">NH</option>
+        <option value="NJ">NJ</option>
+        <option value="NM">NM</option>
+        <option value="NY">NY</option>
+        <option value="NC">NC</option>
+        <option value="ND">ND</option>
+        <option value="OH">OH</option>
+        <option value="OK">OK</option>
+        <option value="OR">OR</option>
+        <option value="PA">PA</option>
+        <option value="RI">RI</option>
+        <option value="SC">SC</option>
+        <option value="SD">SD</option>
+        <option value="TN">TN</option>
+        <option value="TX">TX</option>
+        <option value="UT">UT</option>
+        <option value="VT">VT</option>
+        <option value="VA">VA</option>
+        <option value="WA">WA</option>
+        <option value="WV">WV</option>
+        <option value="WI">WI</option>
+        <option value="WY">WY</option>
+      </InputStyle>
     </Container>
   );
 }
@@ -28,15 +82,15 @@ const Container = styled.div`
   border-radius: 10px;
 `;
 
-const InputStyle = styled.input`
+const InputStyle = styled.select`
   font-family: Lato;
-  color: #000;
+  color: #000;/
   font-size: 18px;
   align-self: stretch;
   flex: 1 1 0%;
   line-height: 16px;
   font-weight: 400;
-  padding: 0px;
+  padding: 5px;
   padding-left: 0px;
   border: none;
   background: transparent;
@@ -47,4 +101,9 @@ const InputStyle = styled.input`
   }
 `;
 
+const OptionStyle = styled.option`
+  font-family: Lato;
+  color: #000;/
+  font-size: 18px;
+`;
 export default TextState;
